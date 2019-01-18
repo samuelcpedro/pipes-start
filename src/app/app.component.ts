9d28a4d5da432b56f1744dd17a55fb863155cc93
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-18 12:02:13 -0100

    Using Pipes
    Using Pipes
    Section 17, Lecture 228

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,8 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+
+  getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
